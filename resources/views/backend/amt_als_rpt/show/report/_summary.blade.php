4a07cbe6e88074a0f0ac73c9eae6a88be851eaf1
jocoonopa <jocoonopa@gmail.com>
2016-12-17 00:00:53 +0800

    報表三圖改為div

--- resources/views/backend/amt_als_rpt/show/report/_summary.blade.php
+++ resources/views/backend/amt_als_rpt/show/report/_summary.blade.php
@@ -1,47 +1,26 @@
-<div class="table-responsive">
-    <table class="table table-borderedr">
-        <tbody>
-            <tr>
-                @if (!is_null($report->cxtBelongs))
-                <th class="text-center" style="width: 33%;">
-                    <img src="/images/backend/als_rpt_ib_cxt/image010.png" class="img-responsive" style="margin: 0 auto;"><br/>
-                    <h2><strong>感觉处理</strong></h2>
-                </th>
-                @endif
-                <th class="text-center" @if(is_null($report->cxtBelongs)) style="width: 50%;"@else style="width: 33%;" @endif>
-                    <img src="/images/backend/als_rpt_ib_cxt/image011.png" class="img-responsive" style="margin: 0 auto;"><br/>
-                    <h2><strong>感觉统合</strong></h2>
-                </th>
-                <th class="text-center" @if(is_null($report->cxtBelongs)) style="width: 50%;"@else style="width: 33%;" @endif>
-                    <img src="/images/backend/als_rpt_ib_cxt/image012.png" class="img-responsive" style="margin: 0 auto;"><br/>
-                    <h2><strong>粗大动作</strong></h2>
-                </th>
-            </tr>
-            <tr>
-                @if (!is_null($report->cxtBelongs))
-                <td class="text-center">
-                    {{$maxAlsCategory->name}}处于
-                    {{ "阈值[{$maxAlsCategory->thread}]-[$maxAlsCategory->type]" }} 
-                </td>
-                @endif
-                <td class="text-center">Level {{$iLevel}}</td>
-                <td class="text-center">Level {{$eLevel}}</td>
-            </tr>
-            <tr>
-                @if (!is_null($report->cxtBelongs))
-                    <td>
-                        <p>{!! nl2br($maxAlsCategory->desc) !!}</p>
-                    </td>
-                @endif
-                <td>
-                    <p>{!! nl2br(\App\Model\AmtCategory::find(\App\Model\AmtCategory::ID_FEEL_INTEGRATE)->commentDescs()->findDescByLevel($iLevel)->first()->desc) !!}
-                    </p>
-                </td>
-                <td>
-                    <p>{!! nl2br(\App\Model\AmtCategory::find(\App\Model\AmtCategory::ID_ROUGH_ACTION)->commentDescs()->findDescByLevel($eLevel)->first()->desc) !!}
-                    </p>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+<div class="row">
+    @if (!is_null($report->cxtBelongs))
+    <div class="col-md-4 col-sm-4 col-xs-12">
+        <img src="/images/backend/als_rpt_ib_cxt/image010.png" class="img-responsive" style="margin: 0 auto;"><hr/>
+        <h2><strong>感觉处理</strong></h2>
+
+        <p>{!! nl2br($maxAlsCategory->desc) !!}</p>
+    </div>
+    @endif
+
+    <div class="col-md-4 col-sm-4 col-xs-12">
+        <img src="/images/backend/als_rpt_ib_cxt/image011.png" class="img-responsive" style="margin: 0 auto;"><hr/>
+        <h2><strong>感觉统合</strong></h2>
+
+        <p>{!! nl2br(\App\Model\AmtCategory::find(\App\Model\AmtCategory::ID_FEEL_INTEGRATE)->commentDescs()->findDescByLevel($iLevel)->first()->desc) !!}
+        </p>
+    </div>
+    <div class="col-md-4 col-sm-4 col-xs-12">
+        <img src="/images/backend/als_rpt_ib_cxt/image012.png" class="img-responsive" style="margin: 0 auto;"><hr/>
+        <h2><strong>粗大动作</strong></h2>
+
+        <p>{!! nl2br(\App\Model\AmtCategory::find(\App\Model\AmtCategory::ID_ROUGH_ACTION)->commentDescs()->findDescByLevel($eLevel)->first()->desc) !!}
+        </p>
+    </div>
 </div>
+    
