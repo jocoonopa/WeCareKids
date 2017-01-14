48710254847302aceb9737586ded182d9f4f20ad
karicktung <karicktung@hotmail.com>
2017-01-14 12:42:46 +0800

    Update _group03.blade.php
    Delete symbol

--- resources/views/frontend/als_rpt_ib_cxt/index/form/_group03.blade.php
+++ resources/views/frontend/als_rpt_ib_cxt/index/form/_group03.blade.php
@@ -1,14 +1,13 @@
 <table class="als-table table table-bordered table-hover table-condensed">
     <thead style="background-color: #e74c3c; color:#fff;">
         <tr>
-            <th colspan="2"><strong>项 目</strong></th>
+            <th><strong>项 目</strong></th>
             <th><strong>三、视觉处理</strong></th>
             <th><strong>選項</strong></th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td class="symbol02">§</td>
             <td>17</td>
             <td>孩子喜欢去五颜六色、通透明亮的地方。</td>
             <td>
@@ -16,7 +15,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>18</td>
             <td>白天在家里，会将窗帘放下或待在能躲避阳光的地方。</td>
             <td>
@@ -24,7 +22,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol02">§</td>
             <td>19</td>
             <td>孩子喜欢色彩艳丽的衣服。</td>
             <td>
@@ -32,7 +29,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol03">○</td>
             <td>20</td>
             <td>当孩子在乱七八糟的抽屉或房间里找东西时，会找不到并产生挫折感。</td>
             <td>
@@ -40,7 +36,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol01">—</td>
             <td>21</td>
             <td>去一个曾经去过的地方时，孩子会对路、地标等感到陌生。</td>
             <td>
@@ -48,7 +43,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol03">○</td>
             <td>22</td>
             <td>看电影或电视时，看到不稳定或快速移动的画面，孩子会觉得不舒服。</td>
             <td>
@@ -56,7 +50,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol01">—</td>
             <td>23</td>
             <td>房间里有其他人走进来时，孩子会察觉不到。</td>
             <td>
@@ -64,7 +57,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>24</td>
             <td>喜欢待在小空间里(如：家里、便利店)，不喜欢或反感嘈杂的大空间(如：家乐福、百货公司这种大空间)。</td>
             <td>
@@ -72,7 +64,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol03">○</td>
             <td>25</td>
             <td>当身边有许多人走动时，孩子会觉得不舒服(如：热闹的购物中心、游行队伍或庆典活动)。</td>
             <td>
@@ -80,7 +71,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>26</td>
             <td>做某件事情的时候，孩子会尽量消除让他容易分心的事情(如：把电视关掉、回到安静的房间)。</td>
             <td>
