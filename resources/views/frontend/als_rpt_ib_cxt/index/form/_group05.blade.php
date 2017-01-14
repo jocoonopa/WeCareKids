b5994911bd8ef1c730a301c72ef340941c4637e7
karicktung <karicktung@hotmail.com>
2017-01-14 12:44:16 +0800

    Delete symbol
    Delete symbol

--- resources/views/frontend/als_rpt_ib_cxt/index/form/_group05.blade.php
+++ resources/views/frontend/als_rpt_ib_cxt/index/form/_group05.blade.php
@@ -1,14 +1,13 @@
 <table class="als-table table table-bordered table-hover table-condensed">
     <thead style="background-color: #16a085; color:#fff;">
         <tr>
-            <th colspan="2"><strong>项 目</strong></th>
+            <th><strong>项 目</strong></th>
             <th><strong>五、活动量</strong></th>
             <th><strong>選項</strong></th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td class="symbol02">§</td>
             <td>40</td>
             <td>孩子会同时做两件以上的事情(如：边听音乐边看绘本)。</td>
             <td>
@@ -16,7 +15,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol01">—</td>
             <td>41</td>
             <td>孩子早上起床时不爱起，容易赖床。</td>
             <td>
@@ -24,7 +22,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol02">§</td>
             <td>42</td>
             <td>做事情比较冲动，一想到什么就马上去做。</td>
             <td>
@@ -32,7 +29,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>43</td>
             <td>孩子会找时间独处，脱离忙碌的生活。</td>
             <td>
@@ -40,7 +36,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol01">—</td>
             <td>44</td>
             <td>孩子做事情的速度比别人慢。</td>
             <td>
@@ -48,7 +43,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol01">—</td>
             <td>45</td>
             <td>对笑话的反应比别人慢，不知道哪里好笑。</td>
             <td>
@@ -56,7 +50,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>46</td>
             <td>孩子会与拥挤的人群保持一定距离。</td>
             <td>
@@ -64,7 +57,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol02">§</td>
             <td>47</td>
             <td>孩子喜欢表现自己(如：唱歌、音乐表演、舞蹈、运动、演讲及在集体中回答问题)。</td>
             <td>
@@ -72,7 +64,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol03">○</td>
             <td>48</td>
             <td>在上课时，孩子容易分心。</td>
             <td>
@@ -80,7 +71,6 @@
             </td>
         </tr>
         <tr>
-            <td class="symbol04">｜</td>
             <td>49</td>
             <td>不喜欢去陌生的地方，或陌生人很多的地方。</td>
             <td>
